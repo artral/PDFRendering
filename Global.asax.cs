b8b5c3d351760db1c1961d551f7881e8ff729994
Artral <artralarasu@gmail.com>
2016-03-05 09:08:14 -0600

    adding script ref

--- Global.asax.cs
+++ Global.asax.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace PDFRendering
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,9 +17,9 @@ namespace PDFRendering
             engine.AddViewLocationFormat("~/PDFRendering/{0}.cshtml");
             engine.AddPartialViewLocationFormat("~/PDFRendering/{0}.cshtml");
             ViewEngines.Engines.Add(engine);
-
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
