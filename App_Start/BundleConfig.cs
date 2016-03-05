3595630ce3f45da73588cee7ee662a83a2bab4b8
Artral <artralarasu@gmail.com>
2016-03-05 09:53:07 -0600

    PIC

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -16,6 +16,9 @@ namespace PDFRendering
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                        "~/Scripts/knockout-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
